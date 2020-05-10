d00b5bf2276af2c35c49beb63b31284f6fdb543e
Achala87 <achala@et.lk>
2020-05-10 17:05:01 +0530

    changes

--- sendmail.php
+++ sendmail.php
@@ -4,18 +4,23 @@ require_once 'google/vendor/googlesheetdata.php'; //gsheetid
   
   // When user submits the form.
   if(isset($_POST)){ 
+     
+    if(empty($_POST["userName"])){
+        $response = array('error');
+        echo json_encode($response); return;
+    } 
 
-    $userName = htmlspecialchars($_POST["userName"]);
-    $userEmail = htmlspecialchars($_POST["userEmail"]);
-    $phoneNumber = htmlspecialchars($_POST["phoneNumber"]);
-    $message = htmlspecialchars($_POST["msg"]);
+    $userName = $_POST["userName"];
+    $userEmail = $_POST["userEmail"];
+    $phoneNumber = $_POST["phoneNumber"];
+    $message = $_POST["msg"];
 
       // Setup email body.
       $body = "<p><b>Name</b> : $userName</p>\n<p><b>Email</b> : $userEmail</p> \n<p><b>phoneNumber</b> : $phoneNumber</p>\n<p><b>Message</b> : $message</p>\n";
 
       // Use PHPMailer class.
       $mail = new PHPMailer();
-      $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
       $mail->isSMTP();
       $mail->SMTPAuth = true;
       $mail->SMTPSecure = 'ssl';
@@ -27,7 +32,7 @@ require_once 'google/vendor/googlesheetdata.php'; //gsheetid
       // Add password of the account
       $mail->Password = $emailpassword;//load from git ignored file above googlesheetdata.php
       
-      $mail->SetFrom(!empty($userEmail) ? $userEmail :'info@reforestsrilanka.com','RFSL Website');
+      $mail->SetFrom(!empty($_POST["userEmail"]) ? $_POST["userEmail"] :'info@reforestsrilanka.com','RFSL Website');
       $mail->Subject = "Web inquiery - Reforest SriLanka";
 
       $mail->Body = $body;
@@ -42,7 +47,7 @@ require_once 'google/vendor/googlesheetdata.php'; //gsheetid
       }
 
       // Email will be send to the user if he filled a valid email.
-      if(!empty($userEmail) and filter_var($userEmail, FILTER_VALIDATE_EMAIL)){
+      if(!empty($_POST["userEmail"])){
         $mailToUser = new PHPMailer();
 
         $mailToUser->isSMTP();
@@ -61,7 +66,7 @@ require_once 'google/vendor/googlesheetdata.php'; //gsheetid
         // Message which should be go to the user.
         $mailToUser->Body = "Thank you for contacting us";
 
-        $mailToUser->AddAddress($userEmail);
+        $mailToUser->AddAddress($_POST["userEmail"]);
         $result = $mailToUser->Send();
         if($result == 1){
           // Done echo "OK Message";
--- trees_include.php
+++ trees_include.php
@@ -10,13 +10,15 @@
     <div class="modal-body">
     <div class="row">
         <div class="col-5">
-        <img src="images/mee.jpg" class="img-fluid rounded" alt="Reforest Sri Lanka Tshirt">
+        <img src="images/mee.jpg" class="img-fluid rounded" alt="Mee Tree">
         </div>
         <div class="col-7">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-        galley of type and scrambled it to make a type specimen book. It has survived not only five 
-        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        <p>Mee/ South-indian mahua is a deciduous tree, scientifically known as Madhuca longifolia
+belongs to Sapotaceae family. It grows in both wet and dry zones in catchment areas and paddy fields.
+Mee tree, grows upto to about 20 m tall. Mee bark, seeds and heartwood are used to treat Urticaria,
+Fracture, Snake-bite poisoning, Rheumatism, Skin diseases, Glandular swelling in the neck &amp; throat in
+Ayurvedic medicine. Can be propagate via seeds. Seed oil and flower honey can be considered as the
+economically valued products provides by Mee tree.</p>
         </div>
 
         <div style="align-content:center;" class="content">    
@@ -41,13 +43,15 @@
     <div class="modal-body">
     <div class="row">
         <div class="col-5">
-        <img src="images/kumbuk.jpg" class="img-fluid rounded" alt="Reforest Sri Lanka Tshirt">
+        <img src="images/kumbuk.jpg" class="img-fluid rounded" alt="Kumbuk Tree">
         </div>
         <div class="col-7">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-        galley of type and scrambled it to make a type specimen book. It has survived not only five 
-        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        <p>Kubuk/ Arjun tree is an evergreen tree, scientifically known as Terminalia arjuna belongs to
+Combretaceae family. It grows up to 30 m tall in both wet and dry Catchment Areas Island wide. It is a
+very large tree with a buttressed, spreading base and horizontally spreading branches. Kubuk bark, leaves,
+roots, flowers and fruits are used to treat Heart diseases, Fevers, Skin diseases, Obesity, Wounds, Bone
+fractures, Dysentery, Piles, Coughs, Leucorrhoea, Candida infections in Ayurvedic medicine. Can be
+propagate via seeds.</p>
         </div>
 
         <div style="align-content:center;" class="content">    
@@ -64,7 +68,7 @@
 <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
 <div class="modal-content">
     <div class="modal-header">
-    <h5 class="modal-title" id="exampleModalLabel">Ahala</h5>
+    <h5 class="modal-title" id="exampleModalLabel">Ehala</h5>
     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
@@ -72,13 +76,15 @@
     <div class="modal-body">
     <div class="row">
         <div class="col-5">
-        <img src="images/ahala.jpg" class="img-fluid rounded" alt="Reforest Sri Lanka Tshirt">
+        <img src="images/ahala.jpg" class="img-fluid rounded" alt="Ehala tree">
         </div>
         <div class="col-7">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-        galley of type and scrambled it to make a type specimen book. It has survived not only five 
-        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        <p>Ehela/ Golden shower tree is a deciduous tree, scientifically known as Cassia fistula belongs to
+Fabaceae family. It can be cultivated in all regions and very common in landscaping as a flowering tree. It
+grows up to about 20 m tall and bloom in yellow. Flowering season lies on February, July- August. Ehela
+roots, bark and the juice of the pods are used to treat Constipation, Nervous system disorders, Oedeama,
+Rashes, Coughs, Burning sensations in the body, Fevers in Ayurvedic medicine. Can be propagate via
+seeds. The seeds of Cassia fistula are a potential commercial source of seed gum.</p>
         </div>
 
         <div style="align-content:center;" class="content">    
@@ -103,13 +109,16 @@
     <div class="modal-body">
     <div class="row">
         <div class="col-5">
-        <img src="images/karanda.jpg" class="img-fluid rounded" alt="Reforest Sri Lanka Tshirt">
+        <img src="images/karanda.jpg" class="img-fluid rounded" alt="Karanda Tree">
         </div>
         <div class="col-7">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-        galley of type and scrambled it to make a type specimen book. It has survived not only five 
-        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        <p>Karada/ Indian beech tree is an evergreen tree, scientifically known as Pongamia pinnata belongs
+to Fabaceae family. It is an indigenous plant which naturalized within both wet and dry zones. Karanda
+planted for shade and as an ornamental. Tree grows to about 15-25 m tall. Karada bark, fruits, roots, root
+bark, leaves, seeds and oil extracted from seeds are used to treat Swelling, Pains, Worm diseases,
+Wounds, Liver diseases, Cough, Skin diseases, Psoriasis, Rheumatoid arthritis, Constipation, Nausea,
+Hemorrhoids, Oral diseases in Ayurvedic medicine. Can be propagate via seeds, branch cuttings and root
+suckers. Seed oil can be considered as the economically valued products by Karada tree.</p>
         </div>
 
         <div style="align-content:center;" class="content">    
@@ -134,13 +143,15 @@
     <div class="modal-body">
     <div class="row">
         <div class="col-5">
-        <img src="images/kohomba.jpg" class="img-fluid rounded" alt="Reforest Sri Lanka Tshirt">
+        <img src="images/kohomba.jpg" class="img-fluid rounded" alt="Kohomba Tree">
         </div>
         <div class="col-7">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-        galley of type and scrambled it to make a type specimen book. It has survived not only five 
-        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        <p>Kohomba/ Neem tree is an evergreen tree, scientifically known as Azadirachta indica belongs to
+Meliaceae family. It can grow about 15 m tall. Kohomba cultivated and naturalized in dry lowland areas
+and able to withstand dry conditions. It is a tall tree with spreading branches, stem and young parts
+glabrous. Kohomba bark, leaves, roots and seeds are used to treat Fevers, Skin ailments, Wounds,
+Coughs, Asthma, Eye diseases, Worms in Ayurvedic medicine. Can be propagate via seeds. Seed oil and
+bark can be considered as the economically valued products by Kohomba tree.</p>
         </div>
 
         <div style="align-content:center;" class="content">    
@@ -165,13 +176,14 @@
     <div class="modal-body">
     <div class="row">
         <div class="col-5">
-        <img src="images/kos.jpg" class="img-fluid rounded" alt="Reforest Sri Lanka Tshirt">
+        <img src="images/kos.jpg" class="img-fluid rounded" alt="Kos Tree">
         </div>
         <div class="col-7">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-        galley of type and scrambled it to make a type specimen book. It has survived not only five 
-        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        <p>Kos/ Jackfruit is an evergreen tree, scientifically known as Artocarpus heterophyllus belongs to
+Moraceae family. It grows up to 25 m in height, cultivated and naturalized within Sri Lanka. Kos leaves,
+latex, heartwood, seeds and fruits is used to treat Irregular bowel syndrome, Diabetes mellitus, Diarrhea,
+Snake bites, Abscess, Dysentery in Ayurvedic medicine. Can be propagate via seeds. Timber and edible
+fruits, seeds can be considered as the economically valued products by Kos tree.</p>
         </div>
 
         <div style="align-content:center;" class="content">    
@@ -196,13 +208,15 @@
     <div class="modal-body">
     <div class="row">
         <div class="col-5">
-        <img src="images/kon.jpg" class="img-fluid rounded" alt="Reforest Sri Lanka Tshirt">
+        <img src="images/kon.jpg" class="img-fluid rounded" alt="Kon Tree">
         </div>
         <div class="col-7">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-        galley of type and scrambled it to make a type specimen book. It has survived not only five 
-        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        <p>Koan is a native tree, scientifically known as Schleichera oleosa belongs to Sapindaceae family.
+It can be found within dry lowland and intermediate forests. Medium to large deciduous tree grows up to
+25 m high. Flowering season from March to April. Koan bark, roots, flowers, leaves and fruits are used to
+treat Malaria, Cholera, Dysentery, Headache, Skin disease, Jaundice in Ayurvedic medicine. Can be
+propagate via seeds. Timber and edible fruits can be considered as the economically valued products by
+Koan tree.</p>
         </div>
 
         <div style="align-content:center;" class="content">    
@@ -215,36 +229,6 @@
 </div>
 </div>
 
-
-<div class="modal fade bd-example-modal-xl" id="kon" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-<div class="modal-content">
-    <div class="modal-header">
-    <h5 class="modal-title" id="exampleModalLabel">Kon</h5>
-    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-    </div>
-    <div class="modal-body">
-    <div class="row">
-        <div class="col-5">
-        <img src="images/tree.jpg" class="img-fluid rounded" alt="Reforest Sri Lanka Tshirt">
-        </div>
-        <div class="col-7">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-        galley of type and scrambled it to make a type specimen book. It has survived not only five 
-        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-        </div>
-
-        <div style="align-content:center;" class="content">    
-            <button class="btn btn-outline-secondary btn-lg" data-toggle="modal" data-target="#orderTrees">Order Trees from Reforest Sri Lanka!</button>
-        </div>
-    </div>  
-    </div>
-</div>
-</div>
-</div>
 
 <div class="modal fade bd-example-modal-xl" id="orderTrees" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
